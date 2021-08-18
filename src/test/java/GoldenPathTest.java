fcf14e80f7dafecd8e118fb20c9d37725a89c461
ilia-pavlov <3472204455ilia@gmail.com>
2021-08-18 11:04:54 -0400

    test/sc

--- src/test/java/GoldenPathTest.java
+++ src/test/java/GoldenPathTest.java
@@ -10,14 +10,19 @@ public class GoldenPathTest extends GoldenPath {
         buyTShirt();
         verifyPurchase(); //here should be assert
 
+/*
+        @Test 2        
+        public void test() { should remove 1 item            
+            openBrowser();            
+            singIn();           
+            buyTShirt();            
+            removePurshaseFromBag(); this will incude -- removeItem(); verifyThatCartIsEmpty(); you have them      
+           verifyPurchase(); //here should be assert -- with empty bag 
+        
+        @Test 3
+        should test diff type of users
 
 
-//        @Test 2
-//        public void test() { should remove 1 item
-//            openBrowser();
-//            singIn();
-//            buyTShirt();
-//            removePurshaseFromBag(); this will incude -- removeItem(); verifyThatCartIsEmpty(); you have them      
-//            verifyPurchase(); //here should be assert -- with empty bag 
+           */
     }
 }
