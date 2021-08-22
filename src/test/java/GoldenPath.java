02f5331bdb4b1a8bee5cf80ba556368b707c6f3a
ThisIs-The-Way <a6465009588@gmail.com>
2021-08-22 14:12:48 -0400

    test/sc

--- src/test/java/GoldenPath.java
+++ src/test/java/GoldenPath.java
@@ -4,68 +4,45 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class GoldenPath extends GoldenPathBase{
 
-    public void verifyPurchase() {
-        new WebDriverWait(driver, 20)
-                .until(ExpectedConditions.visibilityOfElementLocated
-                        (By.xpath("//a[contains(text(),'Gildan G200 Adult Ultra Cotton® 6 oz. T-Shirt–Blac')]")));
-        // accert Expected and Actual                 
-        removeItem();
-        verifyThatCartIsEmpty();
+    public void singIn(){
+        //press sing in button
+        clickButton(By.xpath("//div[@class='header-account__link-container']"));
+        waitForElement(By.xpath("//input[@id='user_email']"));
+
+        //enter email and password
+        sendKey(By.xpath("//input[@id='user_email']"), "0969229149@mail.ru");
+        sendKey(By.xpath("//input[@id='user_password']"), "a969229149");
+        clickButton(By.xpath("//input[@class='primary-button-v3']"));//press sign in button
     }
 
-    public void verifyThatCartIsEmpty() {
-        new WebDriverWait(driver, 20)
-                .until(ExpectedConditions.visibilityOfElementLocated
-                        (By.xpath("//a[contains(text(),'Continue Shopping')]")));
+    public void verifyAction() {
+        waitForElement(By.xpath("//div[@data-product-id='1230']"));
+        verify(By.xpath("//div[@data-product-id='1230']"));
+    }
+
+    public void buyTShirt() {
+        //waitForElement(By.xpath("//div[@data-product-id='1230']"));//wait for the page to loud
+        clickButton(By.xpath("//div[@data-product-id='1230']"));//select item
+        waitForElement(By.xpath("//input[@id='M']"));
+        sendKey(By.xpath("//input[@id='M']"), "23");//select a quantity
+        clickButton(By.xpath("//button[@id='add-to-cart']"));//add to cart
+        waitForElement(By.xpath("//a[@class='button button--cconfirm primary-button-v3']"));
+        clickButton(By.xpath("//a[@class='button button--cconfirm primary-button-v3']"));//view cart/checkout
+    }
+
+    public void verifyPurchase() {
+        waitForElement(By.xpath("//a[contains(text(),'Gildan G200 Adult Ultra Cotton® 6 oz. T-Shirt–Blac')]"));
+        verify(By.xpath("//a[contains(text(),'Gildan G200 Adult Ultra Cotton® 6 oz. T-Shirt–Blac')]"));
     }
 
     public void removeItem() {
-        driver.findElement(By.xpath("//a[contains(text(),'Remove')]")).click();
+        clickButton(By.xpath("//a[contains(text(),'Remove')]"));
         new WebDriverWait(driver, 10).until(ExpectedConditions.alertIsPresent());
         driver.switchTo().alert().accept();
     }
 
-
-    public void buyTShirt() {
-        waitForMainPage();//wait for the page to loud
-        driver.findElement(By.xpath("//div[@data-product-id='1230']")).click();//select item
-        waitForItem();
-        driver.findElement(By.xpath("//input[@id='M']")).sendKeys("23");//select a quantity
-        driver.findElement(By.xpath("//button[@id='add-to-cart']")).click();//add to cart
-        waitForConfirmButton();
-        driver.findElement(By.xpath("//a[@class='button button--cconfirm primary-button-v3']")).click();//view cart/checkout
+    public void verifyThatCartIsEmpty() {
+        waitForElement(By.xpath("//a[contains(text(),'Continue Shopping')]"));
     }
 
-    private void waitForConfirmButton() {
-        new WebDriverWait(driver, 20)
-                .until(ExpectedConditions.visibilityOfElementLocated
-                        (By.xpath("//a[@class='button button--cconfirm primary-button-v3']")));
-    }
-
-    public void singIn(){
-        //press sing in button
-        driver.findElement(By.xpath("//div[@class='header-account__link-container']")).click();
-        waitForEmail();
-
-        //enter email and password
-        driver.findElement(By.xpath("//input[@id='user_email']")).sendKeys("0969229149@mail.ru");
-        driver.findElement(By.xpath("//input[@id='user_password']")).sendKeys("a969229149");
-        driver.findElement(By.xpath("//input[@class='primary-button-v3']")).click();//press sign in button
-    }
-
-
-    public void waitForEmail(){
-        new WebDriverWait(driver, 20)
-                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@id='user_email']")));
-    }
-
-    public void waitForMainPage(){
-        new WebDriverWait(driver, 20)
-                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@data-product-id='1230']")));
-    }
-
-    private void waitForItem() {
-        new WebDriverWait(driver, 20)
-                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@id='M']")));
-    }
 }
--- src/test/java/GoldenPathBase.java
+++ src/test/java/GoldenPathBase.java
@@ -1,11 +1,20 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+
 
 public class GoldenPathBase {
     WebDriver driver;
 
+    @BeforeTest
     public void openBrowser() {
         driver = new FirefoxDriver();
         driver.get("https://www.shirtspace.com/t-shirts");
@@ -15,5 +24,24 @@ public class GoldenPathBase {
     public void closeBrowser() {
         driver.manage().deleteAllCookies();
         driver.close();
+    }
+
+    public void waitForElement(By elementToWait) {
+        ExpectedCondition<WebElement> condition = ExpectedConditions.visibilityOfElementLocated(elementToWait);
+        new WebDriverWait(driver, 10).until(condition);
+    }
+
+    public void clickButton(By elementToClick){
+        driver.findElement(elementToClick).click();
+    }
+
+    public void sendKey(By elementWhereToSend, String textToSend){
+        driver.findElement(elementWhereToSend).sendKeys(textToSend);
+    }
+
+    public void verify(By elementToVerify){
+        WebElement element = driver.findElement(elementToVerify);
+        boolean displayed = element.isDisplayed();
+        Assert.assertTrue(displayed);
     }
 }
